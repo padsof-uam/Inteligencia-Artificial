fb372b1f64be0cad704cde57c58d2c0cc6105456
VicdeJuan <dejuan.victor@gmail.com>
2014-02-18 20:06:03 +0100

    Memoria revisada. Falta el 4

--- P1/ej2.lisp
+++ P1/ej2.lisp
@@ -1,23 +1,14 @@
 (defun combine-elt-lst (elt lst)
 			(mapcar #'(lambda (x) (list elt x)) lst))
 
-;;%% code
-
-
 (defun combine-lst-lst (lst1 lst2)
 			(mapcan #'(lambda (x) (combine-elt-lst x lst2)) lst1))
-
-;;%% code
 
 (defun combine-elt-lst-app (elt lst)
 			(mapcar #'(lambda (x) (cons elt x)) lst))
 
-;;%% code
-
 (defun combine-lst-lst-app (lst1 lst2)
 			(mapcan #'(lambda (x) (combine-elt-lst-app x lst2)) lst1))
-
-;;%% code
 
 (defun combine-lst-of-lst (lst)
 	(if (null (cdr lst))
