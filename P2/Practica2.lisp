d79846c84d5afac67bee10a8310d3924d0ceb646
VicdeJuan <dejuan.victor@gmail.com>
2014-02-19 11:58:08 +0100

    Empecemos

--- P2/Practica2.lisp
+++ P2/Practica2.lisp
@@ -18,4 +18,81 @@
 (setf *sensors*
 '((Avalon 5) (Mallory 7) (Kentares 4) (Davion 1) (Proserpina 4) (Katril 3) (Sirtis 0)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problem definition
+;;
+(defstruct problem
+states 			; List of states
+initial-state 	; Initial state
+fn-goal-test 	; Function (in fn format) that determines; whether a state fulfills the goal
+fn-h 			; Function (in fn format) that evaluates; to the value of the heuristic of a state
+fn-strategy 	; Function (in fn format) that inserts a node in; a list of nodes, according to a specified strategy
+operators) 		; list of operators (in fn format); to generate succesors
+;;
 
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Node in search tree
+;;
+(defstruct node
+state     ; state label
+parent    ; parent node
+action    ; action that generated the current node from its parent
+(depth 0) ; depth in the search tree
+(g 0)     ; cost of the path from the initial state to this node
+h         ; value of the heurstic
+(f 0))    ; g + h
+;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Actions
+;;
+(defstruct action
+Name   ; Name of the operator that generated the action
+origin ; State on which the action is applied
+final  ; State that results from the application of the action
+cost ) ; Cost of the action
+;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Search strategies
+;;
+(defstruct strategy
+name ; Name of the search strategy
+node-compare-p ; boolean comparison
+)
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; fn format for functions
+;;
+
+(defstruct fn
+name 		; Function name
+lst-args) 	; List of additional arguments
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+(defun f-goal-test-galaxy (state planets-destination) ...)
+
+(f-goal-test-galaxy 'Sirtis *planets-destination*) ;-> T
+(f-goal-test-galaxy 'Avalon *planets-destination*) ;-> NIL
+(f-goal-test-galaxy 'Urano *planets-destination*) ;-> NIL
